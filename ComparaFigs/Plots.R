8a67cde0dcb185d6063f5c47875c96bd23cddd16
Alejandro Yañez <aleyanez.rubio@gmail.com>
2020-09-16 19:25:55 -0300

    Agrego figura proyeccion 18 a 20 con capt19 = a 18

--- ComparaFigs/Plots.R
+++ ComparaFigs/Plots.R
@@ -181,4 +181,32 @@ ggsave(p6, filename = "Catch_1820_19parFull_2.png", width=8.5, height=5.5, dpi=3
 
 
 
+# Catch 7 ultimos años 19 full a 2020 (19=18)
+prj18_20_2 <- reptoRlist("../3_2018_2_full/salidas/3_2018_2_full.prj")
+
+p7 <- ggplot(data = NULL, aes(x = c(2014:2020))) + 
+  geom_line(aes(y = c(b19_par$Desemb[1,30:35],b19_par$C_proy[2,3]), colour = 'b19_par', linetype = 'b19_par')) +
+  geom_line(aes(y = c(b19_full$Desemb[1,30:35],b19_full$C_proy[2,3]), colour = 'b19_full', linetype = 'b19_full')) +
+  geom_line(aes(y = c(rep(NA,4),prj18_20$Y_total_proj[1:3,3]), colour = 'prj18_20', linetype = 'prj18_20'))  +
+  geom_line(aes(y = c(rep(NA,4),prj18_20_2$Y_total_proj[1:3,3]), colour = 'prj18_20_2', linetype = 'prj18_20_2'))  +
+  #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
+  
+  scale_color_manual(name = '',
+                     values = c('royalblue3', 'royalblue3', 'red1', 'red1'),
+                     limits = c('b19_par', 'b19_full','prj18_20','prj18_20_2'),
+                     breaks = c('b19_par', 'b19_full','prj18_20','prj18_20_2')) +
+  scale_linetype_manual(name = '',
+                        values = c('dotted', 'solid', 'solid', 'dotted'),
+                        limits = c('b19_par','b19_full', 'prj18_20','prj18_20_2'),
+                        breaks = c('b19_par','b19_full', 'prj18_20','prj18_20_2'))
+p7 <- p7 + theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
+  theme(legend.position = 'bottom') + ylab('Catch') + xlab('Años') + scale_x_continuous(breaks=round(seq(2014, 2020, by = 1),1))
+
+p7
+ggsave(p7, filename = "Catch_1820_19parFull_2.png", width=8.5, height=5.5, dpi=300)
+
+
+
+
 
