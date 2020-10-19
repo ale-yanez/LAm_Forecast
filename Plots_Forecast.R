c74eca37970678ce5223864a7070f65239cf6872
Alejandro Yañez <aleyanez.rubio@gmail.com>
2020-10-19 18:12:52 -0300

    LAm_Norte y proyecciones 2019 a 2021, plots y codigos

--- Plots_Forecast.R
+++ Plots_Forecast.R
@@ -12,24 +12,24 @@ setwd(dirname(current_path ))
 devtools::source_url("https://github.com/ale-yanez/RFunctions/blob/master/read.admb.R?raw=TRUE")
 devtools::source_url("https://github.com/ale-yanez/RFunctions/blob/master/read.report.R?raw=TRUE")
 
+# 2019 2021 opcion 1 ####
 #prj_1  <- reptoRlist("./1_2019_full/salidas/1_2019_full.prj")
-prj_1  <- reptoRlist("./1_2019_full_1/salidas/1_2019_full_1.prj")
+prj_1  <- reptoRlist("./LAm_Norte/1_2019_full_1/salidas/1_2019_full_1.prj")
 
-prj$Rec_male
+prj_1$Rec_male
 
-# Para graficar ... ####
-yrs <- prj$Rec_male[,1]
+# Para graficar ... ##
+yrs <- prj_1$Rec_male[,1]
 M <- 0.3
 lastyr <- tail(yrs, n=1)
 # Brms <- out1$BDoLP*0.4
 # Frms <- out1$Fpbr[3]
 
 
-# Rec_male
-p1 <- ggplot(data = NULL, aes(x = c(yrs, 2020, 2021))) + 
-  geom_line(aes(y = c(prj$Rec_male[,2],rep(NA,2)), colour = 'b_18', linetype = 'b_18')) +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$Rec_male_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
-  #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
+# Rec_total
+p1 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
+  geom_line(aes(y = c(prj_1$Reclutas[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_1$Rec_tot_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
   
   scale_color_manual(name = '',
                      values = c('royalblue3', 'red1'),
@@ -41,16 +41,16 @@ p1 <- ggplot(data = NULL, aes(x = c(yrs, 2020, 2021))) +
                         breaks = c('b_19', 'prj'))
 p1 <- p1 + theme_bw() + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=8)) +
-  theme(legend.position = 'bottom') + ylab('Reclutas machos') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
+  theme(legend.position = 'bottom') + ylab('Reclutas') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
 
 p1
-ggsave(plot_rec, filename = "Rec_male19_21.png", width=7, height=8, dpi=300)
+ggsave(p1, filename = "Figs/2019_2021/Rec19_21_1.png", width=8.5, height=5.5, dpi=300)
 
 
-# Rec_total
+# SSB
 p2 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
-  geom_line(aes(y = c(prj$Reclutas[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$Rec_tot_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  geom_line(aes(y = c(prj_1$SSB[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_1$SSB_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
   #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
   
   scale_color_manual(name = '',
@@ -62,17 +62,17 @@ p2 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) +
                         limits = c('b_19', 'prj'),
                         breaks = c('b_19', 'prj'))
 p2 <- p2 + theme_bw() + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=8)) +
-  theme(legend.position = 'bottom') + ylab('Reclutas') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
+  theme(legend.position = 'bottom') + ylab('SSB') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
 
 p2
-ggsave(p2, filename = "Figs/Rec_2019_21.png", width=8.5, height=5.5, dpi=300)
+ggsave(p2, filename = "Figs/2019_2021/SSB19_21_1.png", width=8.5, height=5.5, dpi=300)
 
 
-# SSB
+# Ftot
 p3 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
-  geom_line(aes(y = c(prj$SSB[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$SSB_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  geom_line(aes(y = c(prj_1$F_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_1$F_total_proy[1:3,3]), colour = 'prj', linetype = 'prj'))  +
   #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
   
   scale_color_manual(name = '',
@@ -85,17 +85,17 @@ p3 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) +
                         breaks = c('b_19', 'prj'))
 p3 <- p3 + theme_bw() + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
-  theme(legend.position = 'bottom') + ylab('SSB') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
+  theme(legend.position = 'bottom') + ylab('F total') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
 
 p3
-ggsave(p3, filename = "Figs/SSB_2019_21.png", width=8.5, height=5.5, dpi=300)
+ggsave(p3, filename = "Figs/2019_2021/F19_21_1.png", width=8.5, height=5.5, dpi=300)
 
 
-# Ftot
+# Catch
 p4 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
-  geom_line(aes(y = c(prj$F_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$F_total_proy[1:3,3]), colour = 'prj', linetype = 'prj'))  +
-  #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
+  geom_line(aes(y = c(prj_1$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_1$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  annotate('text', x=2021.7, y=850, label='922 t') +
   
   scale_color_manual(name = '',
                      values = c('royalblue3', 'red1'),
@@ -107,44 +107,22 @@ p4 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) +
                         breaks = c('b_19', 'prj'))
 p4 <- p4 + theme_bw() + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
-  theme(legend.position = 'bottom') + ylab('F total') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
-
-p4
-ggsave(p4, filename = "Figs/Ftot_2019_21.png", width=8.5, height=5.5, dpi=300)
-
-
-# Catch
-p5 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
-  geom_line(aes(y = c(prj$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
-  #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
-  
-  scale_color_manual(name = '',
-                     values = c('royalblue3', 'red1'),
-                     limits = c('b_19', 'prj'),
-                     breaks = c('b_19', 'prj')) +
-  scale_linetype_manual(name = '',
-                        values = c('solid', 'dotted'),
-                        limits = c('b_19', 'prj'),
-                        breaks = c('b_19', 'prj'))
-p5 <- p5 + theme_bw() + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
   theme(legend.position = 'bottom') + ylab('Catch') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
 
-p5
-ggsave(p5, filename = "Figs/Catch_2019_21.png", width=8.5, height=5.5, dpi=300)
+p4
+ggsave(p4, filename = "Figs/2019_2021/Catch19_21_opt1.png", width=8.5, height=5.5, dpi=300)
 
 
 # Read proyecciones opcion Rec 2
-prj_2 <- reptoRlist("./1_2019_full/salidas/Rec2.prj")
-prj_3 <- reptoRlist("./1_2019_full/salidas/Rec3.prj")
+prj_2 <- reptoRlist("./LAm_Norte/1_2019_full_1/salidas/Rec2.prj")
+prj_3 <- reptoRlist("./LAm_Norte/1_2019_full_1/salidas/Rec3.prj")
 
 # Catch
-p6 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
-  geom_line(aes(y = c(prj$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+p5 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
+  geom_line(aes(y = c(prj_1$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
   #geom_line(aes(y = c(prj_2$Y_total[,2],rep(NA,2)), colour = 'b2_18', linetype = 'b2_18')) +
   #geom_line(aes(y = c(prj_3$Y_total[,2],rep(NA,2)), colour = 'b3_18', linetype = 'b3_18'))# +
-  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_1$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
   geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_2$Y_total_proj[1:3,3]), colour = 'prj2', linetype = 'prj2'))  +
   geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_3$Y_total_proj[1:3,3]), colour = 'prj3', linetype = 'prj3'))  +
   #geom_ribbon(data=NULL, aes(ymin=c(prj$Rec_male[,4],rep(NA,2)), ymax=c(prj$Rec_male[,5],rep(NA,2)), fill = 'grey55', alpha = 0.4)) 
@@ -157,11 +135,74 @@ p6 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) +
                         values = c('solid', 'dotted','dotted','dotted'),
                         limits = c('b_19', 'prj','prj2','prj3'),
                         breaks = c('b_19', 'prj','prj2','prj3'))
+p5 <- p5 + theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
+  theme(legend.position = 'bottom') + ylab('Catch') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
+
+p5
+ggsave(p5, filename = "Figs/2019_2021/Catch_Recs19_21.png", width=8.5, height=5.5, dpi=300)
+
+
+# 2019 2021 opcion 2 ####
+prj_opt2  <- reptoRlist("./LAm_Norte/1_2019_full_2/salidas/1_2019_full_2.prj")
+
+prj_opt2$Rec_male
+
+# Para graficar ...
+yrs <- prj_opt2$Rec_male[,1]
+lastyr <- tail(yrs, n=1)
+
+
+# Catch
+p6 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
+  geom_line(aes(y = c(prj_opt2$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_opt2$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  annotate('text', x=2021, y=850, label='913 t') +
+  
+  scale_color_manual(name = '',
+                     values = c('royalblue3', 'red1'),
+                     limits = c('b_19', 'prj'),
+                     breaks = c('b_19', 'prj')) +
+  scale_linetype_manual(name = '',
+                        values = c('solid', 'dotted'),
+                        limits = c('b_19', 'prj'),
+                        breaks = c('b_19', 'prj'))
 p6 <- p6 + theme_bw() + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
   theme(legend.position = 'bottom') + ylab('Catch') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
 
 p6
-ggsave(p6, filename = "Figs/Catch_Recs2019_21.png", width=8.5, height=5.5, dpi=300)
+ggsave(p6, filename = "Figs/2019_2021/Catch19_21_opt2.png", width=8.5, height=5.5, dpi=300)
 
+# 2019 2021 opcion 3 ####
+
+prj_opt3  <- reptoRlist("./LAm_Norte/1_2019_full_3/salidas/1_2019_full_3.prj")
+
+prj_opt3$Rec_male
+
+# Para graficar ...
+yrs <- prj_opt3$Rec_male[,1]
+lastyr <- tail(yrs, n=1)
+
+
+# Catch
+p7 <- ggplot(data = NULL, aes(x = c(yrs, lastyr+1, lastyr+2))) + 
+  geom_line(aes(y = c(prj_opt3$Y_total[,2],rep(NA,2)), colour = 'b_19', linetype = 'b_19')) +
+  geom_line(aes(y = c(rep(NA,length(yrs)-1),prj_opt3$Y_total_proj[1:3,3]), colour = 'prj', linetype = 'prj'))  +
+  annotate('text', x=2021, y=850, label='969 t') +
+  
+  scale_color_manual(name = '',
+                     values = c('royalblue3', 'red1'),
+                     limits = c('b_19', 'prj'),
+                     breaks = c('b_19', 'prj')) +
+  scale_linetype_manual(name = '',
+                        values = c('solid', 'dotted'),
+                        limits = c('b_19', 'prj'),
+                        breaks = c('b_19', 'prj'))
+p7 <- p7 + theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text=element_text(size=9)) +
+  theme(legend.position = 'bottom') + ylab('Catch') + xlab('Años') + scale_x_continuous(breaks=round(seq(min(yrs), 2020, by = 5),1))
+
+p7
+ggsave(p7, filename = "Figs/2019_2021/Catch19_21_opt3.png", width=8.5, height=5.5, dpi=300)
 
